0bab27a54c55a9a72323709c376e83bd95ecabc6
Daniela <danamattl@yahoo.com>
2024-11-22 18:24:22 +0100

    install router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Projets from './pages/Projects/Projets';
+import Contact from './pages/Contact/Contact';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header />
+      <Routes> 
+        <Route path="/" element= {<Home />}/>
+        <Route path="/projets" element= {<Projets />}/>
+        <Route path="/dashboard" element= {<Contact/>}/>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
