3b0c8dee12cedbfe312326f1f2ee2fc55597912f
Daniela <danamattl@yahoo.com>
2025-01-02 18:11:21 +0100

    update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/projet_12"> 
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
