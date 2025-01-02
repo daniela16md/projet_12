388736e8716939978dd063fd9d98fa310457f014
Daniela <danamattl@yahoo.com>
2025-01-02 18:05:29 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <Router basename="/projet_12">
+    <Router basename={("/")}>
        <ScrollToTop />
       <HeaderDM />
       <Routes> 
-        <Route path="/" element={<Home />} />
-        <Route path="/apropos" element={<Apropos />} />
+        <Route path="/" element= {<Home />}/>
+        <Route path="/apropos" element= {<Apropos />}/>
       </Routes>
       <Footer />
     </Router>
