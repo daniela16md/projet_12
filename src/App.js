1b335ce2f22fbd834d69a520e21c2516c23aacf5
Daniela <danamattl@yahoo.com>
2025-01-02 13:33:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <Router basename={"/"}>
+    <Router>
        <ScrollToTop />
       <HeaderDM />
       <Routes> 
         <Route path="/" element= {<Home />}/>
-        <Route path="/apropos" element= {<Apropos />}/>
+        <Route path="apropos" element= {<Apropos />}/>
       </Routes>
       <Footer />
     </Router>
